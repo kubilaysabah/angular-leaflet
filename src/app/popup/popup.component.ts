0efa4732a076e9be2ad0a16f22a18ab74bafc7c2
Kubilay <kubilaysabah@gmail.com>
2024-12-07 18:44:47 +0300

    refactor popup

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-popup',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.css'
 })
 export class PopupComponent {
+  type: 0 | 1 | 2 = 0;
+
   @Input() heat: number = 0;
 
   @Input() humidity: number = 0;
